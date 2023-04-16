a59ce5f8540d98d23323053026dd052e8c6eed78
Isabelly Santos <isabelly.santos885@gmail.com>
2023-04-16 18:22:34 -0300

    Add files via upload

---
+++ 02-Variaveis.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Programa {
+	static void Main(string[] args) {
+
+		int idade;
+		idade = 21;
+		Console.WriteLine("Minha idade é " + idade);
+
+		idade = 27 - 5;
+		Console.WriteLine(idade);
+
+		idade = 5 * 2 - 6;
+		Console.WriteLine(idade);
+
+		idade = (5 -2) * 3;
+		Console.WriteLine(idade);
+
+		Console.WriteLine("Tecle enter para fechar...");
+		Console.ReadLine();
+	}
+}
+
+// Console.Write = Apenas escreve
+// Console.WriteLine = Escreve e pula linha
---
+++ 03-diasDoMes.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Programa {
+		static void Main(string[] args) {
+		
+			int diasDoMes;
+			diasDoMes = 30;
+			Console.WriteLine("Os dias do mês de abril são " + diasDoMes);
+	}
+}
+
+// C# usa o padrão de criação de nomes de variáveis como CamelCase. 
+// Ex. salarioBruto, minhaIdade, quantidadePecas.
